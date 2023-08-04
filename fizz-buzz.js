f07e852b10a6e7586027f9187c51b7ad71ba7520
NHossen <naeemhossen2509@gmail.com>
2023-08-04 15:20:42 +0400

    Fizz-Buzz game Completed

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -9,18 +9,29 @@
 
 function fizzBuzz(count) {
     for(let i=1; i<=count; i++){
-        console.log(i);
+        if (i%15==0){
+            console.log("FizzBuzz");
+        } 
+        else if(i%3==0){
+          console.log("Fizz");
+        }  
+       
+        else if (i%5==0){
+          console.log("Buzz");
+        }
+         
+        else console.log(i);
     }
-
-
 }
-
-fizzBuzz(15);
-
+fizzBuzz(55);
 
 
-// output.push(1);
-// output.push(2);
-// output.push(3);
-// console.log(output);
+// for (var i = 1; i < 101; i++) {
+//     if (i % 15 == 0) console.log("FizzBuzz");
+//     else if (i % 3 == 0) console.log("Fizz");
+//     else if (i % 5 == 0) console.log("Buzz");
+//     else console.log(i);
+// }
+
+
 
