23a620e7ded41b13d20400e1537b587e53e98879
NHossen <naeemhossen2509@gmail.com>
2023-08-04 10:41:18 +0400

    Number push in empty array

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,5 +1,26 @@
 // Fizz -buzz Solutions by Array...
 
 /*
+1 to 100
 
 */ 
+
+var output=[];
+
+function fizzBuzz() {
+
+    output.push(1);
+
+     console.log(output);
+    
+}
+
+fizzBuzz();
+
+
+
+// output.push(1);
+// output.push(2);
+// output.push(3);
+// console.log(output);
+
